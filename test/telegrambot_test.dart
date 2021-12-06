63a00f247a59bd3a64e7900a36c0e849a2a6366a
Rexfordasamoah51 <rexfordasamoah51@gmail.com>
2021-12-06 18:36:29 +0000

    :white_check_mark:  Added test

--- test/telegrambot_test.dart
+++ test/telegrambot_test.dart
@@ -1,6 +1,37 @@
+/*
+ *  Copyright (c) 2021, Rexford Asamoah Agyapong 
+ * Use of this source code is governed by an MIT-style 
+ * license that can be found in the LICENSE file or at 
+ * https://opensource.org/licenses/MIT.
+ *
+ */
+
 import 'package:telegrambot/telegrambot.dart';
 import 'package:test/test.dart';
 
 void main() {
-  
+  group('Bot Configuration Test', () {
+    test('should return a facts when valid', () async {
+      final expected = await BotConfiguration.facts();
+      expect(expected['facts'], isA<String>());
+    });
+    test('should return a jokes when valid', () async {
+      final expected = await BotConfiguration.jokes();
+      expect(expected['value']['joke'], isA<String>());
+    });
+
+    test('should return an insult when valid', () async {
+      final expected = await BotConfiguration.insult();
+      expect(expected['insult'], isA<String>());
+    });
+    test('should return an advice when valid', () async {
+      final expected = await BotConfiguration.advice();
+      expect(expected['slip']['advice'], isA<String>());
+    });
+
+    test('should return a bible verse when valid', () async {
+      final expected = await BotConfiguration.bible('James 1:1');
+      expect(expected['text'], isA<String>());
+    });
+  });
 }
